70f106788b36489ad8cbc36338d8d7af83c03b5a
rohitth <105496953+Rohitth123@users.noreply.github.com>
2023-08-10 16:29:17 +0530

    ass.js

--- function getSearchVolume() {.js
+++ function getSearchVolume() {.js
@@ -2,7 +2,7 @@ function getSearchVolume() {
     const keyword = document.getElementById("keywordInput").value;
     const API_KEY="AIzaSyDfWlAF-2WmVwOgGK3L_Y8WIC84CE9h1Rk";
 
-    // Make a GET request to your own backend endpoint
+   
     fetch(`https://www.googleapis.com/youtube/v3/search=${encodeURIComponent(keyword)}`)
         .then(response => {
             if (!response.ok) {
@@ -18,4 +18,3 @@ function getSearchVolume() {
         });
 }
 
-// Rest of the code remains the same
